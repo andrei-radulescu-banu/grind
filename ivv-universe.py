be73df2e86f0808ca3fccfd7fa87a1249ba26b98
Andrei Radulescu-Banu <44415560+andrei-radulescu-banu@users.noreply.github.com>
2020-02-02 21:57:25 -0500

    Development

--- ivv-universe.py
+++ ivv-universe.py
@@ -8,6 +8,9 @@ Url = 'https://www.ishares.com/us/products/239726/ishares-core-sp-500-etf/146727
 Date = datetime.datetime.today().strftime('%Y%m%d')
 Data_Top = '{}/data/'.format(os.path.realpath(__file__))
 
+def maybe_download(args, date):
+    print('Downloading {}'.format(date))
+
 if __name__ == "__main__":    
     parser = argparse.ArgumentParser(description='Download S&P500 IVV ETF Universe.')
     parser.add_argument('--url', default=Url, help='Base URL. Default: {}.'.format(Url))
@@ -17,12 +20,16 @@ if __name__ == "__main__":
     parser.add_argument('-f', '--force', action='store_true', help='Overwrite old data, if already downloaded.')
 
     args = parser.parse_args()
+
+    date_last = datetime.datetime.strptime(Date, '%Y%m%d')
     
     if args.all:
         date = datetime.datetime(2011, 12, 30)
-        date1 = datetime.datetime.strptime(Date, '%Y%m%d')
         
-        while date <= date1:
-            print(date)
+        while date <= date_last:
+            maybe_download(args, date)
             date += datetime.timedelta(days=1)
+            
+    else:
+        maybe_download(args, date_last)
             
