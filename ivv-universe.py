e661ff4bb1c85b51d501ad6170e4992d9076f049
Andrei Radulescu-Banu <44415560+andrei-radulescu-banu@users.noreply.github.com>
2020-02-02 21:40:35 -0500

    Development

--- ivv-universe.py
+++ ivv-universe.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
 
-import sys
+import sys, os
 import argparse
+
+Url = 'https://www.ishares.com/us/products/239726/ishares-core-sp-500-etf/1467271812596.ajax?fileType=csv&fileName=IVV_holdings&dataType=fund&asOfDate='
+Data_Top = '{}/data/'.format(os.path.realpath(__file__))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Download S&P500 IVV ETF Universe.')
-    parser.add_argument('--url', default='https://www.ishares.com/us/products/239726/ishares-core-sp-500-etf/1467271812596.ajax?fileType=csv&fileName=IVV_holdings&dataType=fund&asOfDate=', help='Base URL. ')
+    parser.add_argument('--url', default=Url, help='Base URL. Default: {}'.format(Url))
     parser.add_argument('-d', '--date', default='', help='Date in YYYYMMDD format.')
+    parser.add_argument('-t', '--top', default=Data_Top, help='Where to save the data. Default: {}'.format(Data_Top))
 
     args = parser.parse_args()
     args.func(args)
