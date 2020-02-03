325f15a05369e3fafaa15c529001965544d71ccc
Andrei Radulescu-Banu <44415560+andrei-radulescu-banu@users.noreply.github.com>
2020-02-02 21:46:55 -0500

    Development

--- ivv-universe.py
+++ ivv-universe.py
@@ -14,8 +14,10 @@ if __name__ == "__main__":
     
     parser = argparse.ArgumentParser(description='Download S&P500 IVV ETF Universe.')
     parser.add_argument('--url', default=Url, help='Base URL. Default: {}.'.format(Url))
-    parser.add_argument('-d', '--date', default='', help='Date in YYYYMMDD format. Default: {}'.format(Date))
+    parser.add_argument('-d', '--date', default=Date, help='Date in YYYYMMDD format. Default: {}'.format(Date))
     parser.add_argument('-t', '--top', default=Data_Top, help='Where to save the data. Default: {}'.format(Data_Top))
+    parser.add_argument('-a', '--all', action='store_true', help='Download all preceding dates.')
+    parser.add_argument('-f', '--force', action='store_true', help='Overwrite old data, if already downloaded.')
 
     args = parser.parse_args()
     args.func(args)
