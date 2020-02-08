f7e7afba1f3fdd776ac6157d40e04ce26483fb42
Andrei Radulescu-Banu <44415560+andrei-radulescu-banu@users.noreply.github.com>
2020-02-08 11:02:48 -0500

    Development

--- ivv-universe.py
+++ ivv-universe.py
@@ -3,33 +3,14 @@
 import sys, os
 import argparse
 import datetime
+import pandas
+import numpy as np
 
-Url = 'https://www.ishares.com/us/products/239726/ishares-core-sp-500-etf/1467271812596.ajax?fileType=csv&fileName=IVV_holdings&dataType=fund&asOfDate='
-Date = datetime.datetime.today().strftime('%Y%m%d')
-Data_Top = '{}/data/'.format(os.path.realpath(__file__))
-
-def maybe_download(args, date):
-    print('Downloading {}'.format(date))
+DirDefault = '/home/andrei/src/market-data/ivv'
 
 if __name__ == "__main__":    
-    parser = argparse.ArgumentParser(description='Download S&P500 IVV ETF Universe.')
-    parser.add_argument('--url', default=Url, help='Base URL. Default: {}.'.format(Url))
-    parser.add_argument('-d', '--date', default=Date, help='Date in YYYYMMDD format. Default: {}'.format(Date))
-    parser.add_argument('-t', '--top', default=Data_Top, help='Where to save the data. Default: {}'.format(Data_Top))
-    parser.add_argument('-a', '--all', action='store_true', help='Download all preceding dates.')
-    parser.add_argument('-f', '--force', action='store_true', help='Overwrite old data, if already downloaded.')
+    parser = argparse.ArgumentParser(description='Generate S&P500 Universe based on IVV ETF.')
+    parser.add_argument('--dir', default=DirDefault, help='Directory of IVV csv files. Default: {}.'.format(DirDefault))
 
     args = parser.parse_args()
 
-    date_last = datetime.datetime.strptime(Date, '%Y%m%d')
-    
-    if args.all:
-        date = datetime.datetime(2011, 12, 30)
-        
-        while date <= date_last:
-            maybe_download(args, date)
-            date += datetime.timedelta(days=1)
-            
-    else:
-        maybe_download(args, date_last)
-            
