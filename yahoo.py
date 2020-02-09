fa79cfdfd7ffcc8ec9743068c6905455937e61dc
Andrei Radulescu-Banu <44415560+andrei-radulescu-banu@users.noreply.github.com>
2020-02-08 22:42:56 -0500

    Development

--- yahoo.py
+++ yahoo.py
@@ -13,6 +13,7 @@ if __name__ == "__main__":
     parser.add_argument('-t', '--ticker', nargs='+', help='Ticker')
     parser.add_argument('--dir', default=DirDefault, help='Directory of IVV csv files. Default: {}.'.format(DirDefault))
     parser.add_argument('-d', '--debug', action='store_true', help='Enable debug messages.')
+    parser.add_argument('-f', '--force', action='store_true', help='Force a download even if the data is cached.')
 
     args = parser.parse_args()
 
@@ -20,8 +21,33 @@ if __name__ == "__main__":
     date_str = date.strftime("%Y%m%d")
     
     for ticker in args.ticker:
+        # Will be saving the data in this file name
         fname = '{}/{}_{}.csv'.format(args.dir, ticker, date_str)
-        print(fname)
-        #        yticker = yf.Ticker(ticker)
 
+        # Check for old data
+        if not args.force:
+            if os.path.exists(fname):
+                if args.debug:
+                    print('{} already available'.format(fname))
+                    continue
+
+        # Remove old data
+        if os.path.exists(fname):
+            os.unlink(fname)
+
+        # Create the yf ticker object
+        yticker = yf.Ticker(ticker)
+
+        # Download the max history
+        df = yticker.history(period="max")
+
+        # Check if we downloaded anything
+        if df.empty:
+            continue
+
+        # Save to file
+        df.to_csv(fname)
+        
+        if args.debug:
+            print('Saved {}'.format(fname))
     
